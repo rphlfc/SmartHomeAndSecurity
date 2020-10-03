de1345d6a20a5212f59371d8e2bf07fd2681a718
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-10-02 23:37:03 -0300

    Initial Commit

---
+++ SmartHomeAndSecurity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartHomeAndSecurity
+//
+//  Created by Raphael Cerqueira on 02/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartHomeAndSecurity/SmartHomeAndSecurityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeAndSecurityApp.swift
+//  SmartHomeAndSecurity
+//
+//  Created by Raphael Cerqueira on 02/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeAndSecurityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
